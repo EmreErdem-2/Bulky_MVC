b9eea829a13d166dbcc76c3015ed29b4c3c98db2
Emre Erdem <emreerdem95@outlook.com>
2025-06-03 01:31:17 +0300

    category index and migration

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -9,23 +9,23 @@ namespace BulkyWeb.Data
         {
         }
         public DbSet<Category> Categories { get; set; }
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    modelBuilder.Entity<BulkyWeb.Models.Category>().HasData(
-        //        new BulkyWeb.Models.Category
-        //        {
-        //            Id = 1,
-        //            Name = "Action",
-        //            DisplayOrder = 1
-        //        },
-        //        new BulkyWeb.Models.Category
-        //        {
-        //            Id = 2,
-        //            Name = "Drama",
-        //            DisplayOrder = 2
-        //        }
-        //    );
-        //    base.OnModelCreating(modelBuilder);
-        //}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "Action",
+                    DisplayOrder = 1
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Drama",
+                    DisplayOrder = 2
+                }
+            );
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
