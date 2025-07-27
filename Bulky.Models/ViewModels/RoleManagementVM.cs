5ca9c1da598d2a34b28b7c0588b6689218485891
Emre Erdem <emreerdem95@outlook.com>
2025-07-27 05:13:46 +0300

    User roles and permissions change. Also can specify company information

---
+++ Bulky.Models/ViewModels/RoleManagementVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulkyBook.Models.ViewModels
+{
+    public class RoleManagementVM
+    {
+        public ApplicationUser User { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> RoleList { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> CompanyList { get; set; }
+    }
+}
--- BulkyWeb/Areas/Admin/Controllers/UserController.cs
+++ BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -1,9 +1,11 @@
 ﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository;
 using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
 using BulkyBook.Models.ViewModels;
 using BulkyBook.Utility;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -15,13 +17,92 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
     public class UserController : Controller
     {
         private readonly ApplicationDbContext _applicationDb;
-        public UserController(ApplicationDbContext applicationDb)
+        private readonly IUnitOfWork _unitOfWork;
+        public UserController(ApplicationDbContext applicationDb, IUnitOfWork unitOfWork)
         {
             _applicationDb = applicationDb;
+            _unitOfWork = unitOfWork;
         }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult RoleManagement(string? userId)
+        {
+            if (userId == null || userId == "")
+            {
+                return NotFound();
+            }
+            var userFromDb = _unitOfWork.ApplicationUser.Get(u => u.Id == userId, includeProperties: "Company");
+            if (userFromDb == null)
+            {
+                return NotFound();
+            }
+            userFromDb.Role = _applicationDb.UserRoles
+                .Where(u => u.UserId == userFromDb.Id)
+                .Select(u => _applicationDb.Roles.FirstOrDefault(r => r.Id == u.RoleId).Name)
+                .FirstOrDefault() ?? string.Empty; // Default to empty if no role found
+            RoleManagementVM roleManagementVM = new RoleManagementVM()
+            {
+                User = userFromDb,
+                RoleList = _applicationDb.Roles.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Name
+                }).ToList(),
+                CompanyList = _applicationDb.Company.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }).ToList()
+            };
+            return View(roleManagementVM);
+        }
+
+        [HttpPost]
+        public IActionResult RoleManagement(RoleManagementVM roleManagementVM)
+        {
+            var userFromDb = _unitOfWork.ApplicationUser.Get(u => u.Id == roleManagementVM.User.Id, includeProperties: "Company");
+            var currentUserRoleId = _applicationDb.UserRoles.FirstOrDefault(u => u.UserId == userFromDb.Id).RoleId;
+            var newRoleId = _applicationDb.Roles.FirstOrDefault(u => u.Name == roleManagementVM.User.Role).Id;
+
+            if (roleManagementVM.User.Role != null && currentUserRoleId != newRoleId)
+            {
+                // Update UserRole table
+                var userRoleFromDb = _applicationDb.UserRoles.FirstOrDefault(u => u.UserId == userFromDb.Id);
+                if (userRoleFromDb != null)
+                {
+                    _applicationDb.UserRoles.Remove(userRoleFromDb);
+                    _applicationDb.SaveChanges();
+
+                    var newUserRole = new IdentityUserRole<string>
+                    {
+                        UserId = userFromDb.Id,
+                        RoleId = newRoleId
+                    };
+                    _applicationDb.UserRoles.Add(newUserRole);
+                    _applicationDb.SaveChanges();
+                }
+                else
+                {
+                    TempData["error"] = "No User found for Role";
+                }
+            }
+            if (roleManagementVM.User.Company != null)
+            {
+                userFromDb.CompanyId = roleManagementVM.User.Company.Id;
+                _unitOfWork.Save();
+            }
+            else
+            {
+                userFromDb.CompanyId = null;
+                _unitOfWork.Save();
+            }
+            
+            TempData["success"] = "User role updated!";
+            
+            return RedirectToAction("Index");
         }
 
         #region API Calls
